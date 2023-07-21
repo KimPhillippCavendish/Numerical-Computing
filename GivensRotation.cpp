a675e6cfe570e6d24380d55ce2dbb770c096fa60
KimPhillippCavendish <57471669+KimPhillippCavendish@users.noreply.github.com>
2023-07-21 14:09:36 +0200

    Update GivensRotation.cpp

--- GivensRotation.cpp
+++ GivensRotation.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Eigen/Dense"
+#include "Eigen.hpp"
 // Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms.
 // https://eigen.tuxfamily.org/index.php?title=Main_Page
 
