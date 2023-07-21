5844495d5e422899299f96d46861fe93f5fe8ce2
KimPhillippCavendish <57471669+KimPhillippCavendish@users.noreply.github.com>
2023-07-21 14:10:48 +0200

    Create Eigen.hpp

---
+++ Eigen.hpp
@@ -0,0 +1,20 @@
+// info@stce.rwth-aachen.de
+
+#pragma once
+
+#include "Eigen/Dense"
+
+namespace Eigen {
+
+  template<typename T, int N=Dynamic>
+  using vector_t=Matrix<T,N,1>;
+
+  template<typename T, int N=Dynamic>
+  using row_vector_t=Matrix<T,1,N>;
+
+  template<typename T, int M=Dynamic, int N=M>
+  using matrix_t=Matrix<T,M,N>;
+
+}
+
+ 
